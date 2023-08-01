225e1f1359081c7f9f6c984354980c8341130aef
Renan Martins <fagundesm22@gmail.com>
2023-07-31 22:33:42 -0300

    Added e2e test for all endpoints of user domain.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -35,12 +35,41 @@ describe('AppController (e2e)', () => {
     await app.close();
   });
 
+  it('Create User', () => {
+    const payload = {
+      name: 'New New User',
+      email: 'new_my_user@domain.com',
+      password: 'ultimate_hard_password',
+    };
+
+    return request(app.getHttpServer())
+      .post('/users')
+      .send(payload)
+      .expect(201);
+  });
+
   it('Get All Users', () => {
     return request(app.getHttpServer()).get('/users').expect(200);
   });
 
   it('Get User By ID', () => {
     return request(app.getHttpServer()).get('/users/1').expect(200);
+  });
+
+  it('Update User', () => {
+    const payload = {
+      name: 'User Updated',
+      email: 'user_updated@domain.com',
+      password: 'complex_Password',
+    };
+    return request(app.getHttpServer())
+      .put('/users/1')
+      .send(payload)
+      .expect(200);
+  });
+
+  it('Delete User by ID', () => {
+    return request(app.getHttpServer()).delete('/users/1').expect(200);
   });
 
   it('/health (GET)', () => {
