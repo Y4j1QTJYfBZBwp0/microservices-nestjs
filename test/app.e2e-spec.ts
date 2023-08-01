d556e33f3dd68075eb84920be04edccf68831631
Renan Martins <fagundesm22@gmail.com>
2023-07-31 22:18:44 -0300

    Added and configured Testcontainers to user docker-compose.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,32 +2,45 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import {
+  DockerComposeEnvironment,
+  StartedDockerComposeEnvironment,
+} from 'testcontainers';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  let environment: StartedDockerComposeEnvironment;
+
+  beforeAll(async () => {
+    const composeFilePath = '';
+    const composeFile = 'docker-compose.yaml';
+
+    environment = await new DockerComposeEnvironment(
+      composeFilePath,
+      composeFile,
+    ).up();
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
     await app.init();
   });
 
-  it('/users (GET)', () => {
-    return request(app.getHttpServer()).get('/users').expect(200).expect([]);
+  afterAll(async () => {
+    await environment.down();
+    await app.close();
   });
 
-  it('/health (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/health')
-      .expect(200)
-      .expect({ status: 'ok' });
+  it('Get All Users', () => {
+    return request(app.getHttpServer()).get('/users').expect(200);
   });
 
-  it('/users (GET)', () => {
-    return request(app.getHttpServer()).get('/users').expect(200).expect([]);
+  it('Get User By ID', () => {
+    return request(app.getHttpServer()).get('/users/1').expect(200);
   });
 
   it('/health (GET)', () => {
